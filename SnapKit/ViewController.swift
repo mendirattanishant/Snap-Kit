a777027fe3ed402b1ec735fd99e4fb26101a494b
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2018-04-06 13:26:10 -0700

    Add a new button using snapkit.

--- SnapKit/ViewController.swift
+++ SnapKit/ViewController.swift
@@ -14,6 +14,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let superview = self.view
+        let button = UIButton()
+        button.layer.cornerRadius = 33
+        button.setTitle("+", for: .normal)
+        button.backgroundColor = UIColor.black
+        superview?.addSubview(button)
+        button.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(66)
+            make.height.equalTo(66)
+            if let bottom = superview?.snp.bottom {
+                make.bottom.equalTo(bottom).offset(-20)
+            }
+            if let right = superview?.snp.right {
+                make.right.equalTo(right).offset(-20)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
